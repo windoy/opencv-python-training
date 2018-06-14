54c5923ed0a6c6410df419ddece7b07c93fa64b3
Erwin <erwin.d.chavez@accenture.com>
2018-06-14 17:21:56 +0800

    day two training

--- notebooks/3-image-filters.py
+++ notebooks/3-image-filters.py
@@ -13,7 +13,8 @@ def apply_invert(frame):
 
 def apply_sepia(frame, intensity=0.5):
     blue, green, red = 20, 66, 112
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    frame = apply_alpha_covert(frame)
     frame_height, frame_width, frame_channel = frame.shape
     
     sepia_bgra = (blue, green, red, 1);
@@ -39,6 +40,33 @@ def apply_green(frame, intensity=0.5):
     
     return frame  
 
+def apply_alpha_covert(frame):
+    try:
+        frame.shape[3]
+    except IndexError:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+    return frame    
+    
+    
+def apply_portrait_mode(frame):
+    frame = apply_alpha_covert(frame)
+    # frame_height, frame_width, frame_channel = frame.shape
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    _, mask = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY)
+    
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGRA)
+    blurred = cv2.GaussianBlur(frame, (21, 21), 0)
+    
+    blended = apply_blend(frame, blurred, mask)
+    frame = cv2.cvtColor(blended, cv2.COLOR_BGRA2BGR)
+    
+    return frame
+
+def apply_blend(frame_1, frame_2, mask):
+    alpha = mask / 255.0
+    blended = cv2.convertScaleAbs(frame_1 * (1 - alpha) + (frame_2 * alpha))
+    return blended
+    
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -47,11 +75,12 @@ while True:
     invert = apply_invert(frame)
     sepia = apply_sepia(frame)
     green = apply_green(frame)
+    portrait = apply_portrait_mode(frame)
     
     cv2.imshow('frame', frame)
     cv2.imshow('invert', invert)
     cv2.imshow('sepia', sepia)
-    cv2.imshow('green', green)
+    cv2.imshow('portrait', portrait)
     
     k = cv2.waitKey(1)
     
